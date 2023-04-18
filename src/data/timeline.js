32569a0b5ffc798e9a6d6f3586778af4b972b68e
fredblaise <fred.blaise4@gmail.com>
2023-04-18 01:48:31 -0400

    update to timeline

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -1,26 +1,26 @@
 export default [
 	{
 		year: 'Today',
-		title: 'Bachelor\'s Degree in Computer Science',
+		title: "Bachelor's Degree in Computer Science",
 		details:
-			'I realized that I had a lot more to learn about programming and development so I decided to go back to school. I do believe education is very important and will lead to have a greater understanding of computer science fundamentals that would aide me in my career.',
+			'I realized that I had a lot more to learn about programming and development so I decided to go back to school. I do believe education is very important and will lead me to have a greater understanding of computer science fundamentals that will aide me in my career.',
 	},
 	{
 		year: 'Today',
 		title: 'Configuration Engineer',
 		details:
-			"I'm a solo indie developer building software for myself and clients to showcase on YouTube. All coding projects are built from the ground up, from planning and designing all the way to solving real-life problems with code.",
+			"This position allows me configure production environments for enterprise clients and be adjacent to other software engineers. This experience sharpened my skills in dealing with production web applications and pushed me to learn the inner workings of the software tech stack and tooling. Taking what I learn at this position and what I learn on my own, I'm able to develop the necessary skills to add value to organizations.",
 	},
 	{
 		year: '2021',
 		title: "Bachelor's Degree in Business Administration",
 		details:
-			'I pursued the knowledge that a computer science degree had to offer. I learned about computer architecture, computer science theory, an array of programming, and aspects of artificial intelligence.',
+			'I have a natural interest in business that started since I was young. Reading case studies of the business decisions of various companies has always fascinated me and has shaped how I work and what I decide to work on. I analyze many of the technologies I choose to learn through the skills that I cultivated while attaining this degree.',
 	},
 	{
 		year: '2017',
 		title: 'First Line of Code',
 		details:
-			"I wrote my first line of real code that started this journey into digital craftsmanship that I never could've imagined, especially as someone who never saw themselves as anything other than a blue collar worker.",
+			"I was thrown into the world of code through many assignment and personal projects. At first, it was confusing and overwhelming. However, I had developed a passion for development that continues to today.",
 	},
 ]
