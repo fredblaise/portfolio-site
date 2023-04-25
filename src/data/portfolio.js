4a195215a2e0afbfbbf3deeac7d478bbe08518f0
fredblaise <fred.blaise4@gmail.com>
2023-04-25 04:34:42 -0400

    rename notes img

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,7 +1,7 @@
 export default [
    {
       title: 'Notes App',
-      imgUrl: 'public/assets/notes-app.png',
+      imgUrl: 'public/assets/notes-app-background.png',
       stack: ['React', 'Tailwind', 'Headless UI', 'Vite'],
       link: 'https://notes.fredblaise.com',
       details: '',
