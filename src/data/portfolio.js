2474247394fc4f9b0e30240447c74ce17f0458d6
fredblaise <fred.blaise4@gmail.com>
2023-04-25 04:32:15 -0400

    fix notes img again

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,21 +1,21 @@
 export default [
    {
       title: 'Notes App',
-      imgUrl: '../public/assets/notes-app.PNG',
+      imgUrl: 'public/assets/notes-app.png',
       stack: ['React', 'Tailwind', 'Headless UI', 'Vite'],
       link: 'https://notes.fredblaise.com',
       details: '',
    },
    {
       title: 'Project 2',
-      imgUrl: 'http://via.placeholder.com/640x360',
+      imgUrl: 'https://via.placeholder.com/640x360',
       stack: ['React', 'Tailwind', 'Vite'],
       link: 'https://google.com',
       details: 'This is the description for the portfolio item.',
    },
    {
       title: 'Project 3',
-      imgUrl: 'http://via.placeholder.com/640x360',
+      imgUrl: 'https://via.placeholder.com/640x360',
       stack: ['Python', 'C++', 'Robotics'],
       link: 'https://google.com',
       details: 'This is the description for the portfolio item.',
