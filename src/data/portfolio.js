71bc3bd9b03a1b11e575cc0aa318ca803f6af4e8
fredblaise <fred.blaise4@gmail.com>
2023-05-17 19:31:44 -0400

    update notes app pic

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,14 +1,7 @@
 export default [
    {
-      title: 'Django To-Do App',
-      imgUrl: '/assets/todo-app.jpeg',
-      stack: ['Python', 'Django', 'PostgreSQL', 'Bootstrap'],
-      link: 'https://todo-app-production-e6e6.up.railway.app/',
-      details: '',
-   },
-   {
       title: 'Notes App',
-      imgUrl: '/assets/notes-app-background.png',
+      imgUrl: '/assets/notes-app-pic.png',
       stack: ['React', 'Tailwind', 'Headless UI', 'Vite'],
       link: 'https://notes.fredblaise.com',
       details: '',
@@ -18,6 +11,13 @@ export default [
       imgUrl: '/assets/trivia-app.png',
       stack: ['React', 'Tailwind', 'Vite'],
       link: 'https://trivia.fredblaise.com',
+      details: '',
+   },
+   {
+      title: 'Django To-Do App',
+      imgUrl: '/assets/todo-app.jpeg',
+      stack: ['Python', 'Django', 'PostgreSQL', 'Bootstrap'],
+      link: 'https://todo-app-production-e6e6.up.railway.app/',
       details: '',
    },
 ]
